4b24ba83c799255a458e0d8eb42b2de020892f32
frannpineroo <frannpinero@hotmail.com>
2026-04-23 16:30:41 -0300

    Creando el route auth y agregandolo al index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const io = new Server(httpServer, {
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Fintrack API corriendo ' })
