045689ecea9c21742428bc004241a076429c9401
frannpineroo <frannpinero@hotmail.com>
2026-04-28 20:01:34 -0300

    Creo ruta nueva para ver gastos totales del user

--- backend/src/routes/expense.routes.ts
+++ backend/src/routes/expense.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createExpense, getGroupExpenses, markSplitAsPaid } from '../controllers/expense.controller'
+import { createExpense, getGroupExpenses, markSplitAsPaid, getMyExpenses } from '../controllers/expense.controller'
 import { authMiddleware } from '../middlewares/auth.middleware'
 
 const router = Router()
 
-router.post('/', authMiddleware, createExpense)
-router.get('/group/:groupId', authMiddleware, getGroupExpenses)
-router.put('/splits/:splitId/paid', authMiddleware, markSplitAsPaid)
+router.post('/', authMiddleware, createExpense);
+router.get('/group/:groupId', authMiddleware, getGroupExpenses);
+router.put('/splits/:splitId/paid', authMiddleware, markSplitAsPaid);
+router.get('/me', authMiddleware, getMyExpenses);
 
 export default router
